77eb25ca2da1bdd491ce31f9ffe4e3ad97b951a0
Brick <Brickabrat87@yahoo.com>
2024-01-22 21:05:48 -0500

    Day 13

---
+++ Day13.py
@@ -0,0 +1,11 @@
+# Write a program to shuffle the elements of a list randomly.
+import random
+
+def random_shuffle():
+
+    list_to_shuffle = input("Please enter values with ' ' delimiter:  ").split()
+    random.shuffle(list_to_shuffle)
+    return list_to_shuffle
+
+
+print(random_shuffle())
